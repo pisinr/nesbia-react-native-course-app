287017eea2075c6d5526e9ef4cf8e4f5cf66a6e1
Pisin R <pisin.r@gmail.com>
2019-04-27 00:40:36 +0700

    Sample image

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet, Text, View,
+  Image
+} from 'react-native';
+
+import profileImage from './assets/profile.png';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.title}>Hello</Text>
+        <Image style={styles.image} source={profileImage} />
+        <Text style={styles.text}>Icon from FreePik!</Text>
       </View>
     );
   }
@@ -14,8 +21,25 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ddd',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    backgroundColor: '#eee',
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    borderWidth: 2,
+    borderColor: '#666',
+  },
+  title: {
+    fontSize: 48,
+    fontWeight: 'bold',
+    color: '#3a3',
+    padding: 5,
+  },
+  text: {
+    color: '#3a3',
   },
 });
